8ee82ec3be7fcc68f5cb72bdfc9e5f77a85ab9b6
Debonex <719893090@qq.com>
2020-07-14 11:26:36 +0800

    @Debonex +caea_20200714_71js_73js_74js_75js

---
+++ 73.矩阵置零.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=73 lang=javascript
+ *
+ * [73] 矩阵置零
+ *
+ * https://leetcode-cn.com/problems/set-matrix-zeroes/description/
+ *
+ * algorithms
+ * Medium (55.65%)
+ * Likes:    251
+ * Dislikes: 0
+ * Total Accepted:    45.6K
+ * Total Submissions: 82K
+ * Testcase Example:  '[[1,1,1],[1,0,1],[1,1,1]]'
+ *
+ * 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 
+ * [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * 输出: 
+ * [
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]
+ * ]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 
+ * [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * 输出: 
+ * [
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
+ * 
+ * 进阶:
+ * 
+ * 
+ * 一个直接的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+ * 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+ * 你能想出一个常数空间的解决方案吗？
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+    let width = matrix[0].length
+    let height = matrix.length
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (matrix[i][j] == 0) {
+                for (let k = 0; k < width; ++k)(k != j) && (matrix[i][k] != 0) && (matrix[i][k] = 'todo')
+                for (let k = 0; k < height; ++k)(k != i) && (matrix[k][j] != 0) && (matrix[k][j] = 'todo')
+            }
+        }
+    }
+    for (let i = 0; i < height; i++)for (let j = 0; j < width; j++)matrix[i][j] == 'todo' && (matrix[i][j] = 0)
+};
+// @lc code=end
+
